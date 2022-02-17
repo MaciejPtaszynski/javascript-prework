5a2884a8dba420d1bc25a0dd3eadcd5cb8597952
Maciej Ptaszynski <mpsendit@gmail.com>
2022-02-17 11:46:50 +0100

    add player move

--- js/script.js
+++ js/script.js
@@ -22,4 +22,19 @@ function printMessage(msg){
       computerMove = 'nieznany ruch';
     }
     printMessage('Mój ruch: ' + computerMove);
-}
+
+    var playerInput, playerMove;
+    playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+    console.log('Wpisana odpowiedź to: ' + playerInput);
+    if (playerInput == '1') {
+    playerMove = 'kamień';
+    } else if (playerInput == '2') {
+    playerMove = 'papier';
+    } else if (playerInput == '3') {
+    playerMove = 'nożyce';
+    } else {
+    playerMove = 'nieznany ruch';
+    printMessage('nieprawidłowe zagranie');
+    }
+    printMessage('Twój ruch: ' + playerMove);
+    }
